dff46945a8e86d35fd4ba480a23d8d5758354cc5
felix2feng <felix2feng@gmail.com>
2019-05-23 20:07:55 -0700

    Export managers

--- artifacts/index.ts
+++ artifacts/index.ts
@@ -1,1 +1,22 @@
-// Empty file
+export { BTCDaiRebalancingManager } from './ts/BTCDaiRebalancingManager';
+export { BTCETHRebalancingManager } from './ts/BTCETHRebalancingManager';
+export { ETHDaiRebalancingManager } from './ts/ETHDaiRebalancingManager';
+export { DailyPriceFeed } from './ts/DailyPriceFeed';
+export { FeedFactory } from './ts/FeedFactory';
+export { LinkedListLibraryMock } from './ts/LinkedListLibraryMock';
+export { ManagerLibraryMock } from './ts/ManagerLibraryMock';
+export { MovingAverageOracle } from './ts/MovingAverageOracle';
+export { PriceFeed } from './ts/PriceFeed';
+
+// Export abi-gen contract wrappers
+export {
+	BTCDaiRebalancingManagerContract,
+	BTCETHRebalancingManagerContract,
+	ETHDaiRebalancingManagerContract,
+	DailyPriceFeedContract,
+	FeedFactoryContract,
+	LinkedListLibraryMockContract,
+	ManagerLibraryMockContract,
+	MovingAverageOracleContract,
+	PriceFeedContract,
+} from "../utils/contracts";
