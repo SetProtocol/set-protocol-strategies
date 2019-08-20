e1a5401555388457ebb16dda694c84db48a30ba5
bweick <brian.weickmann@gmail.com>
2019-08-20 11:09:54 -0700

    Bumped package version.

--- artifacts/index.ts
+++ artifacts/index.ts
@@ -3,11 +3,14 @@ export { BTCETHRebalancingManager } from './ts/BTCETHRebalancingManager';
 export { ETHDaiRebalancingManager } from './ts/ETHDaiRebalancingManager';
 export { FeedFactory } from './ts/FeedFactory';
 export { HistoricalPriceFeed } from './ts/HistoricalPriceFeed';
-export { LinkedListLibraryMock } from './ts/LinkedListLibraryMock';
+export { LegacyMakerOracleAdapter } from './ts/LegacyMakerOracleAdapter';
 export { LinearizedPriceDataSource } from './ts/LinearizedPriceDataSource';
 export { MACOStrategyManager } from './ts/MACOStrategyManager';
+export { MACOStrategyManagerV2 } from './ts/MACOStrategyManagerV2';
 export { ManagerLibraryMock } from './ts/ManagerLibraryMock';
 export { MovingAverageOracle } from './ts/MovingAverageOracle';
+export { MovingAverageOracleV2 } from './ts/MovingAverageOracleV2';
+export { OracleProxy } from './ts/OracleProxy';
 export { PriceFeed } from './ts/PriceFeed';
 export { TimeSeriesFeed } from './ts/TimeSeriesFeed';
 
@@ -19,11 +22,15 @@ export {
 	ETHDaiRebalancingManagerContract,
 	FeedFactoryContract,
 	HistoricalPriceFeedContract,
+	LegacyMakerOracleAdapterContract,
 	LinkedListLibraryMockContract,
 	LinearizedPriceDataSourceContract,
 	MACOStrategyManagerContract,
+	MACOStrategyManagerV2Contract,
 	ManagerLibraryMockContract,
 	MovingAverageOracleContract,
+	MovingAverageOracleV2Contract,
+	OracleProxyContract,
 	PriceFeedContract,
 	TimeSeriesFeedContract,
 } from "../utils/contracts";
