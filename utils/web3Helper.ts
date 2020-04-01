2edafc2d91515169bbc7b872b8f15e425d30eafe
Justin Chen <justin.chen@airbnb.com>
2020-04-01 00:31:41 -0400

    Bump versions and get rid of build folder in package

--- utils/web3Helper.ts
+++ utils/web3Helper.ts
@@ -80,7 +80,7 @@ export const importArtifactsFromSource = (contractName: string) => {
   } catch (e) {}
 
   try {
-    const data = require('set-protocol-strategies/build/contracts/' + contractName + '.json');
+    const data = require('set-protocol-strategies/dist/artifacts/ts/' + contractName + '.js')[contractName];
     instance = contract(data);
     instance.setProvider(web3.currentProvider);
 
@@ -88,8 +88,8 @@ export const importArtifactsFromSource = (contractName: string) => {
   } catch (e) {}
 
   try {
-    const filePath = 'set-protocol-strategies-' + version + '/build/contracts/' + contractName + '.json';
-    const data = require(filePath);
+    const filePath = 'set-protocol-strategies-' + version + '/dist/artifacts/ts/' + contractName + '.js';
+    const data = require(filePath)[contractName];
     instance = contract(data);
     instance.setProvider(web3.currentProvider);
 
